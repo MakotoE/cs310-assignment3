91b4ebd57753c334d71f928251740e97f354f17c
Makoto <makoto@emurasoft.com>
2021-07-30 11:52:37 -0700

    correlation_using_stdlib()

--- assn3.py
+++ assn3.py
@@ -1,4 +1,6 @@
+from statistics import stdev, mean
 from typing import List
+from itertools import combinations
 
 import numpy as np
 import csv
@@ -15,5 +17,29 @@ def read_prices(name: str) -> List[float]:
 	return result
 
 
+def correlation_using_stdlib(x_prices: List[float], y_prices: List[float]) -> float:
+	x_mean = mean(x_prices)
+	y_mean = mean(y_prices)
+	x_stddev = stdev(x_prices)
+	y_stddev = stdev(y_prices)
+
+	numerator = 0
+	for x, y in zip(x_prices, y_prices):
+		numerator += (x - x_mean) * (y - y_mean)
+
+	return numerator / ((len(x_prices) - 1) * x_stddev * y_stddev)
+
+
 if __name__ == '__main__':
-	print(read_prices('AAPL'))
+	symbols = ['AAPL', 'AMZN', 'FB', 'GOOG', 'IBM', 'MSFT', 'NFLX', 'ORCL', 'SAP', 'TSLA']
+	stock_prices = [(s, read_prices(s)) for s in symbols]
+
+	print('non-NumPy version')
+	correlations = [
+		((x_name, y_name), correlation_using_stdlib(x_prices, y_prices))
+		for ((x_name, x_prices), (y_name, y_prices)) in combinations(stock_prices, 2)
+	]
+	correlations.sort(key=lambda item: item[1], reverse=True)
+
+	for ((x_name, y_name), correlation) in correlations:
+		print(f'{x_name}:{y_name} = {correlation}')
